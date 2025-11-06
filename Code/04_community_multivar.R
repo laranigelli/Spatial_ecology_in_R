1cf1752801900dd75f1795573be2750558f7bfc3
laranigelli <lara.nigelli@studio.unibo.it>
2025-11-06 17:47:17 +0100

    Create 04_community_multivar.R

---
+++ Code/04_community_multivar.R
@@ -0,0 +1,9 @@
+# code for perfoming multivariate analysis with community abundance matrices
+install.packages("vegan")
+library(vegan)
+
+# inside this package there is dune data, representing the dune system
+data(dune)
+
+# if we have a hufe dataset, we can use the head function that shows the first six rows. 
+head(dune)
